15c807e36c5ffe574b9cb4fff6ebdfbcc5f902be
mhanestgm <mhanes@student.tgm.ac.at>
2023-10-02 18:58:59 +0200

    GSON als Gradle-Dependency hinzugefügt

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     implementation("org.json:json:20230618")
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("commons-validator:commons-validator:1.7")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
